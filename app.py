511137035c1bcb5d8f6263861f6ceda259790b9c
Death-and-Strawberry <saniity9@gmail.com>
2026-06-09 13:56:56 +0100

    Creation of main home page and tempates for many of the pages that will be included, creating a connection  to the db and adding the reservation table, added some logic and a basic form for the booking page, yet to hook up to the db as users and other tables are needed. CSS for the pages included, add as you go along

--- app.py
+++ app.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from db import *
+
 from routes.auth import auth
 from routes.dashboard import dashboard_route
 
 app = Flask(__name__)
 app.secret_key = "boo"
 
+mysql = get_connection()
+
+
 app.register_blueprint(auth)
 app.register_blueprint(dashboard_route)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
 # fucntions for view menu etc stuff that dont need user to be logged in
 
+@app.route("/bookings", methods=["GET", "POST"])
+def bookings():
+    #get form input
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    phone = request.form.get("phone")
+    date = request.form.get("date")
+    time = request.form.get("time")
+    guests = request.form.get("guests")
+
+    
+    return render_template("bookings.html")
+
+@app.route("/submitbooking")
+def submit_booking():
+    return render_template("submit_booking.html")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5005, debug=True)
--- db.py
+++ db.py
@@ -1,10 +1,34 @@
 import mysql.connector
+import os 
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root@123",
-        database="testproject",
-        port=3306
+        host = os.getenv("host")
+        user = os.getenv("user"),
+        password = os.getenc("password"),
+        database = os.getenv("database"),
+        port = os.getenv("port")
     )
+
+
+def create_booking():
+    conn = get_connection()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS reservations(
+                reservation_id INT PRIMARY KEY AUTO_INCREMENT,
+                user_id INT NOT NULL,
+                table_id INT,
+                booking_date DATE NOT NULL,
+                booking_time TIME NOT NULL,
+                size INT NOT NULL,
+                status VARCHAR(30) NOT NULL DEFAULT 'booked',
+
+                CONSTRAINT fk_reservations_user
+                    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+
+                CONSTRAINT fk_reservations_table
+                    FOREIGN KEY (table_id) REFERENCES RestaurantTables(table_id)
+            );
+        ''')
