66924fdcb6f3bb33eef53ab3dd745848a80a8579
Ben Atkinson <142601279+benatkinsonx@users.noreply.github.com>
2026-06-10 16:18:25 +0100

    voucher logic works! required adding cost_multiplier field to Rewards table

--- routes/checkout.py
+++ routes/checkout.py
@@ -10,15 +10,52 @@ checkout = Blueprint("checkout", __name__)
 @checkout.route("/gotocheckout", methods=["POST"])
 def gotocheckout():
     session['total'] = 78
-    total = session.get('total', 0)
-    return render_template('voucher.html', total=total, message="voucher is valid/invalid")
+    total = session['total']
+    return render_template('voucher.html', total=total, message="")
 
-@checkout.route('/voucher', methods=['GET','POST'])
+@checkout.route('/voucher', methods=['POST'])
 def voucher():
     if "user_id" not in session:
         return redirect(url_for("auth.login"))
     
-    return render_template('delivery.html')
+    try:
+        db = get_connection()
+        cursor = db.cursor()
+
+        voucher_code = request.form.get('voucher_code')
+
+        voucher_name_query = """
+            SELECT reward_name
+            FROM Rewards
+            WHERE reward_name = %s AND active = TRUE
+    """
+        cursor.execute(voucher_name_query, (voucher_code,))
+        voucher_name_result = cursor.fetchone()
+    
+        if voucher_name_result:
+            cost_multiplier_query = """
+                SELECT cost_multiplier
+                FROM Rewards
+                WHERE reward_name = %s AND active = TRUE
+            """
+            cursor.execute(cost_multiplier_query, (voucher_code,))
+            cost_multiplier_result = cursor.fetchone()
+            session['discounted_total'] = session['total'] * cost_multiplier_result[0]
+            return render_template('delivery.html', total=session['discounted_total'], message=f"Voucher '{voucher_code}' applied successfully! £{session['total']:.2f} reduced to £{session['discounted_total']:.2f}")
+        else:
+            return render_template('voucher.html', total=session['total'], message="Invalid voucher code. Try again or proceed without a voucher.")
+    
+    except mysql.connector.Error as err:
+        return f"""
+            <h3>Database Error!</h3>
+            <p>{err}</p>
+            <a href="/login">Try Again</a>
+        """
+    finally:
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
 
 @checkout.route('/delivery', methods=['POST'])
 def delivery():
